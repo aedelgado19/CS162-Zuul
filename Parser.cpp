be1a9477c97bbfb3f429d18cd00af498725b0d80
aedelgado19 <aedelgado19@gmail.com>
2020-10-27 13:38:09 -0700

    check valid works

--- Parser.cpp
+++ Parser.cpp
@@ -15,17 +15,120 @@ Command Parser::getCommand(){
     cout << "> ";
     cin.get(inputLine, 50);
     cin.get();
-    parseCommand(inputLine);
+    mytoken = parseCommand(inputLine);
     checkValid(mytoken);
   }
 }
 
 bool Parser::checkValid(tokens* mytoken){
 
-
+  //go commands
+  if(strcmp(mytoken->word1, "go") == 0){
+    if(strcmp(mytoken->word2, "north") == 0){
+      return true;
+    }
+    else if(strcmp(mytoken->word2, "east") == 0){
+      return true;
+    }
+    else if(strcmp(mytoken->word2, "south") == 0){
+      return true;
+    }
+    else if(strcmp(mytoken->word2, "west") == 0){
+      return true;
+    }
+    else if(strcmp(mytoken->word2, "up") == 0){
+      return true;
+    }
+    else if(strcmp(mytoken->word2, "down") == 0){
+      return true;
+    }
+    else{
+      cout << "Second word not valid" << endl;
+      return false;
+    }
+  }
+  //quit
+  if(strcmp(mytoken->word1, "quit") == 0){
+    return true;
+  }
+  //help
+  if(strcmp(mytoken->word1, "help") == 0){
+    return true;
+  }
+  //get
+  if(strcmp(mytoken->word1, "get") == 0){
+    if(strcmp(mytoken->word2, "jalapenos") == 0){
+      return true;
+    }
+    else if(strcmp(mytoken->word2, "turkey") == 0){
+      return true;
+    }
+    else if(strcmp(mytoken->word2, "cheese") == 0){
+      return true;
+    }
+    else if(strcmp(mytoken->word2, "mayo") == 0){
+      return true;
+    }
+    else if(strcmp(mytoken->word2, "mayonnaise") == 0){
+      return true;
+    }
+    else if(strcmp(mytoken->word2, "tomatoes") == 0){
+      return true;
+    }
+    else if(strcmp(mytoken->word2, "lettuce") == 0){
+      return true;
+    }
+    else {
+      cout << "get what?" << endl;
+      return false;
+    }   
+  }
+  //drop items
+  if(strcmp(mytoken->word1, "drop") == 0){
+    if(strcmp(mytoken->word2, "jalapenos") == 0){
+      return true;
+    }
+    else if(strcmp(mytoken->word2, "turkey") == 0){
+      return true;
+    }
+    else if(strcmp(mytoken->word2, "cheese") == 0){
+      return true;
+    }
+    else if(strcmp(mytoken->word2, "mayo") == 0){
+      return true;
+    }
+    else if(strcmp(mytoken->word2, "mayonnaise") == 0){
+      return true;
+    }
+    else if(strcmp(mytoken->word2, "tomatoes") == 0){
+      return true;
+    }
+    else if(strcmp(mytoken->word2, "lettuce") == 0){
+      return true;
+    }
+    else {
+      cout << "drop what?" << endl;
+      return false;
+    }
+  }
+  //inventory
+  if(strcmp(mytoken->word1, "inventory") == 0){
+    return true;
+  }
+  if(strcmp(mytoken->word1, "sandwich") == 0){
+    return true;
+  }
+  
+  if(strcmp(mytoken->word1, "eat") == 0){
+    cout << "no, you cannot anything, it's not lunch time!" << endl;
+    return false;
+  }
+  cout << "unknown command." << endl;
+  return false;
 }
 
 tokens* Parser::parseCommand(char* inputLine){
+
   char *charPtr;
   tokens *mytoken = new tokens;
   charPtr = strtok(inputLine, " ");
